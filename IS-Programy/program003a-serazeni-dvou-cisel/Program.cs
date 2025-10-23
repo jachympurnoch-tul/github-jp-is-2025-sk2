7d8484f4891e750a55b67e7657dc9ce04e885cb8
Jáchym Purnoch <jachym.purnoch@tul.cz>
2025-10-23 13:12:18 +0200

    #7 Porovnávání čísel

--- IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("---------- Seřazení dvou čísel ---------");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("---------- Výpis číselné řady ----------");
+    Console.WriteLine("------ Jáchym Purnoch / 23.10.2025 -----");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine();
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Děláte machry, ale hajzl máte na chodbě! (Nezedali jste celé číslo A. Zadejte celé číslo znovu!)");
+    }
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Děláte machry, ale hajzl máte na chodbě! (Nezedali jste celé číslo B. Zadejte celé číslo znovu!)");
+    }
+
+    Console.WriteLine();
+
+    int pom;
+    //Chceme seřadit čísla vzestupně
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných");
+    }
+
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Seřazená čísla: {a}, {b} ");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+}
